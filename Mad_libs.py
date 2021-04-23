4a082843c24ef4d68cd3d7f063487794bc7090d7
Aurimas Butvilauskas <a.butvilauskas@gmail.com>
2021-04-23 12:56:32 +0300

    Update Mad_libs.py

--- Mad_libs.py
+++ Mad_libs.py
@@ -1,30 +1,35 @@
 def istorija_m():
-    print('Istorija Moteris')
+    print('\t \n Sveiki, aš ' + vardas + '. Man yra ' + amzius +
+          ' metai. \n Vaikų neturiu. Esu kilusi iš ' + miestas + '.')
 
 
 def istorija_mv():
-    print('Istorija moteris su vaikais')
+    if vaiku_kiekis == '1':
+        print('\t \n Sveiki, aš ' + vardas + '. Man yra ' + amzius +
+              ' metai. \n Turiu ' + vaiku_kiekis + ' vaiką. Esu kilusi iš ' + miestas + '.')
+    else:
+        print('\t \n Sveiki, aš ' + vardas + '. Man yra ' + amzius +
+              ' metai. \n Turiu ' + vaiku_kiekis + ' vaikus. Esu kilusi iš ' + miestas + '.')
 
 
 def istorija_v():
-    print('Istorija vyras')
+    print('\t \n Sveiki, aš ' + vardas + '. Man yra ' + amzius +
+              ' metai. \n Vaikų neturiu. Esu kilęs iš ' + miestas + '.')
 
 
+vaiku_skaicius = ['1', '2', '3', '4', '5']
 vardas = input('Koks tavo vardas?- ')
-try:
-    age = int(input('Kiek tau metų?- '))
-except ValueError:
-    print('Amžių rašykite skaičiais!')
+amzius = input('Kiek tau metų?- ')
 miestas = input('Iš kokio tu miesto?- ')
 vaikai = input('Ar turite vaikų? (T/N)- ').upper()
 if vaikai == 'T':
-    vaiku_kiekis = int(input('Iveskite vaikų skaičių: '))
+    vaiku_kiekis = input('Iveskite vaikų skaičių: ')
 else:
-    vaiku_kiekis = 0
+    vaiku_kiekis = '0'
 lytis = input('Tu moteris (M) ar vyras (V)?- ').upper()
-if lytis == 'M' and vaiku_kiekis == 0:
+if lytis == 'M' and vaiku_kiekis == '0':
     istorija_m()
-elif lytis == 'M' and vaiku_kiekis > 0:
+elif lytis == 'M' and vaiku_kiekis in vaiku_skaicius:
     istorija_mv()
 else:
     istorija_v()
