23e01ade5c90ef1ef2ff221aa524c897bf5fc129
Aurimas Butvilauskas <a.butvilauskas@gmail.com>
2021-04-23 16:08:28 +0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,18 @@
 import random
 
+didzioji1 = chr(random.randint(65, 90))
+didzioji2 = chr(random.randint(65, 90))
+didzioji3 = chr(random.randint(65, 90))
+mazoji1 = chr(random.randint(97, 122))
+mazoji2 = chr(random.randint(97, 122))
+mazoji3 = chr(random.randint(97, 122))
+skaicius1 = str(random.randint(1, 9))
+skaicius2 = str(random.randint(1, 9))
+skaicius3 = str(random.randint(1, 9))
+simbolis1 = chr(random.randint(33, 47))
+simbolis2 = chr(random.randint(33, 47))
+simbolis3 = chr(random.randint(33, 47))
+
 
 def ilgumas():
     ilgis = int(input('Keliu simboliu slaptazodis? [8,10 ar 12]? - '))
@@ -21,57 +34,35 @@ def ilgumas():
         else:
             return 'Neskaitote salygos!'
 
+
 def maisymas(stringas):
     stringas = list(stringas)
     random.shuffle(stringas)
     password = ''.join(stringas)
     return password
 
+
 def generacija_a():
-    didzioji1 = chr(random.randint(65, 90))
-    didzioji2 = chr(random.randint(65, 90))
-    mazoji1 = chr(random.randint(97, 122))
-    mazoji2 = chr(random.randint(97, 122))
-    skaicius1 = str(random.randint(1, 9))
-    skaicius2 = str(random.randint(1, 9))
-    simbolis1 = chr(random.randint(33, 47))
-    simbolis2 = chr(random.randint(33, 47))
-    password = didzioji1 + didzioji2 + mazoji1 + mazoji2 + skaicius1 + skaicius2 + simbolis1 + simbolis2
+    password = didzioji1 + didzioji2 + mazoji1 + mazoji2 \
+               + skaicius1 + skaicius2 + simbolis1 + simbolis2
     password = maisymas(password)
     return password
+
 
 def generacija_b():
-    didzioji1 = chr(random.randint(65, 90))
-    didzioji2 = chr(random.randint(65, 90))
-    didzioji3 = chr(random.randint(65, 90))
-    mazoji1 = chr(random.randint(97, 122))
-    mazoji2 = chr(random.randint(97, 122))
-    skaicius1 = str(random.randint(1, 9))
-    skaicius2 = str(random.randint(1, 9))
-    skaicius3 = str(random.randint(1, 9))
-    simbolis1 = chr(random.randint(33, 47))
-    simbolis2 = chr(random.randint(33, 47))
-    password = didzioji1 + didzioji2 + didzioji3 + mazoji1 + mazoji2 + skaicius1 + skaicius2 + skaicius3 + simbolis1 + simbolis2
+    password = didzioji1 + didzioji2 + didzioji3 + mazoji1 \
+               + mazoji2 + skaicius1 + skaicius2 + skaicius3 \
+               + simbolis1 + simbolis2
     password = maisymas(password)
     return password
 
+
 def generacija_c():
-    didzioji1 = chr(random.randint(65, 90))
-    didzioji2 = chr(random.randint(65, 90))
-    didzioji3 = chr(random.randint(65, 90))
-    mazoji1 = chr(random.randint(97, 122))
-    mazoji2 = chr(random.randint(97, 122))
-    mazoji3 = chr(random.randint(97, 122))
-    skaicius1 = str(random.randint(1, 9))
-    skaicius2 = str(random.randint(1, 9))
-    skaicius3 = str(random.randint(1, 9))
-    simbolis1 = chr(random.randint(33, 47))
-    simbolis2 = chr(random.randint(33, 47))
-    simbolis3 = chr(random.randint(33, 47))
-    password = didzioji1 + didzioji2 + didzioji3 + mazoji1\
-               + mazoji2 + mazoji3 + skaicius1 + skaicius2\
+    password = didzioji1 + didzioji2 + didzioji3 + mazoji1 \
+               + mazoji2 + mazoji3 + skaicius1 + skaicius2 \
                + skaicius3 + simbolis1 + simbolis2 + simbolis3
     password = maisymas(password)
     return password
+
 
 print(ilgumas())
